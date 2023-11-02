2a64a88fc0d2355f639f65d97211b080e9d33299
jayendra singh <145662877+jayendrasingh22@users.noreply.github.com>
2023-11-02 12:33:13 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,28 @@
+# def  my_function():
+#     x=10             #x is a global variable
+#     print(x)           #accessigning the gloabal
+
+# my_function()
+
+
+
+
+#2)
+# def function():
+#     x=1
+   
+#     print(x)
+# function()
+
+#non-local global
+# x=10             #global variable
+# def modify_global():
+#     global x
+#     x=20
+#     modify_global()
+
+
+
+
+
+
