64ef82d167fd24a67c96481fabb0406c5571834e
jayendra singh <145662877+jayendrasingh22@users.noreply.github.com>
2023-11-02 12:54:39 +0530

    Add files via upload

---
+++ game_guess.py
@@ -0,0 +1,18 @@
+import random
+def guess_number():
+    return random.randint(1, 100)
+
+target_number = guess_number()
+attempts = 0
+
+while True:
+    user_guess = int(input("Enter number: "))
+    attempts += 1
+
+    if user_guess == target_number:
+        print("Congratulation! You guessed the number in", attempts, "attempts.")
+        break
+    elif user_guess < target_number:
+        print("Try a higher number.")
+    else:
+        print("Try a lower number.")
