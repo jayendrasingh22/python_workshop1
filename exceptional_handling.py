8ef8183507339458468a1d392a0e798102141f06
jayendra singh <145662877+jayendrasingh22@users.noreply.github.com>
2023-11-03 11:27:12 +0530

    Add files via upload

---
+++ exceptional_handling.py
@@ -0,0 +1,46 @@
+
+# errro occurs when excecution of program is stopped
+
+# how to handel the error ,(what are exceptional handling ?)
+
+# types of error
+
+
+# 1.syntax errror :refers to formal rules governing the construction of valid statement in a language
+                #    
+
+#ex:  
+# def main()          # colon :  missing
+#     a=10
+#     b=3
+#     print("sum is",a+b    # ) missing
+
+
+# 2.semantic error: 
+# guitar plays rama  
+
+
+# 3.run time error :  occurs during execution of programme, it is caused because of some illegal operation taking place.
+#ex:
+# if a program is trying to open a progrsm which doesnot exixt or it could not be opened,results in execution werror
+# 2.an expression is trying to divide sa num by zero are run time error
+
+
+# 4.logical error : i is the error which is caused a programm to produce incorrect or undisered output.
+# for instance,
+
+# ctr=1;
+# while(ctr<10):
+#     print(n*ctr)
+
+
+
+
+
+
+# what is exception?
+#  ans # even if a ststement or sn expression is syntacticaly correct,it may cuse an error when an attempt is made to execute it.error detetced during execution ar ecalled exceptions.
+
+# 4+spam*3
+
+
